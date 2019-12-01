f024ccce7a6cf853003a854f6a1fe61bb2b082e9
aelphias <igormitchell11@gmail.com>
2019-12-01 12:48:25 +0300

    insert() can insert

--- fillit/backtrack.c
+++ fillit/backtrack.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 14:09:14 by aelphias          #+#    #+#             */
-/*   Updated: 2019/11/30 16:21:43 by aelphias         ###   ########.fr       */
+/*   Updated: 2019/12/01 12:46:54 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,28 @@ void clean_map(char map[16][17])
     }
     
 }
-void    insert(char abc, int *coord, char map[16][17], int y, int x)
+int    insert(char abc, int *coord, char map[16][17], int y, int x)
 {   
     int i;
      
     i = 0;
-    map[0][0] = abc;
-    while (i < 6)
-    {
-        map[y + coord[i]][x + coord[i + 1]] = abc;
-        i = i +2 ;
-    }
+    if (map[0][0] == '.')
+        map[0][0] = abc;
+    else
+        return(0);
     
+    while (i < 6 )
+    {
+        if (map[y + coord[i]][x + coord[i + 1]] == '.')
+        {
+            
+            map[y + coord[i]][x + coord[i + 1]] = abc;
+            i = i + 2 ;
+        }
+        else
+            return(0);
+    }
+    return (1);
 }
 
 void    backtrack(int *count, int storage[26], char map[16][17])
@@ -64,23 +74,19 @@ void    backtrack(int *count, int storage[26], char map[16][17])
     y = 0;
     col = 0;
     row = 0;
-       // row = coord[6]; // using 2 last pairs from storage array to have safe boundaries
     abc = 'A';
     printf("backtrack abc=%c\n", abc);
-    coord = g_tetramines[*count];
-    //printf("backtrack coord =%s\n", coord);
-        printf("backtrack fig coordintes = ");
-	 i = 0;
+    coord = g_tetramines[storage[*count]];
+    printf("backtrack fig coordintes = ");
+	i = 0;
      while (i < 6)
     {
         printf("%d", g_tetramines[*count][i]);
         i++;
     }
-        printf("\n");
-    
+    printf("\n");
     printf("backtrack map =%s\n", map[0]);
     printf("backtrack storage =%c\n", storage[0]);
-       // a = min_a(count);
     a = 0;
     printf("a=%d\n",a);
     insert(abc, coord, map, y, x);
@@ -93,4 +99,4 @@ void    backtrack(int *count, int storage[26], char map[16][17])
             }
             col++;
         } */
-}
+} 
