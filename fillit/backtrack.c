892819a90b44e4a2acccdb8ee88d1856fcdea14c
aelphias <igormitchell11@gmail.com>
2019-12-02 12:05:55 +0300

    doesn't print

--- fillit/backtrack.c
+++ fillit/backtrack.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 14:09:14 by aelphias          #+#    #+#             */
-/*   Updated: 2019/12/01 16:25:41 by aelphias         ###   ########.fr       */
+/*   Updated: 2019/12/02 12:05:04 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int min_a(int *count)
     i++;
     return (i - 1);
 }
+
 void clean_map(char map[16][17])
 {
      int i;
@@ -31,32 +32,48 @@ void clean_map(char map[16][17])
         ft_memset(map[i], '.', 16);
         i++;
     }
-    
 }
+
 int    insert(char abc, int *coord, char map[16][17], int y, int x, int a)
 {   
     int i;
+    int j;
      
     i = 0;
-    while (x <= a - coord[9] && y <=a - coord[8])
+    if(x <= a - coord[9] && y <=a - coord[8])
     {
-    if (map[y][x] == '.')
-        map[y][x] = abc;
-    else
-        return(0);
-    
-    while (i < 6 )
-    {
-        if (map[y + coord[i]][x + coord[i + 1]] == '.')
-        {
-             map[y + coord[i]][x + coord[i + 1]] = abc;
-            i = i + 2 ;
-        }
+        if (map[y][x] == '.')
+            map[y][x] = abc;
         else
             return(0);
+        while (i < 6 )
+        {
+            if (map[y + coord[i]][x + coord[i + 1]] == '.')
+            {
+                 map[y + coord[i]][x + coord[i + 1]] = abc;
+                i = i + 2 ;
+            }
+            else
+                return(0);
+        }
     }
+    else
+    {
+        insert(abc, coord, map, y, x, a + 1);
+    	i = 0;
+    }
+	while (i <= a)
+	{
+		j = 0;
+		while (j <= a)
+		{
+			ft_putchar(map[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
     return (1);
-    }
 }
 
 void    backtrack(int *count, int storage[26], char map[16][17])
@@ -70,22 +87,17 @@ void    backtrack(int *count, int storage[26], char map[16][17])
     int x;
     int y;
     int i;
-    int j;
     int flg;
-   // int back;
  
 	coord = g_tetramines[storage[*count - 1]]; /* [storage[*count - 1] is a number  of figure 0-18 */
     i = 0;
     flg = 0;
-    //back = 1;
     x = coord[7];
     y = coord[6];
     col = 0;
     row = 0;
     abc = 'A';
     printf("backtrack abc=%c\n", abc);
-    //printf("backtrack coord =%s\n", coord);
-        printf("backtrack fig coordinates = ");
 	 i = 0;
      while (i < 6)
     {
@@ -98,7 +110,8 @@ void    backtrack(int *count, int storage[26], char map[16][17])
   	 a = min_a(count);
     printf("a=%d\n",a);
    i = 0;
-    while(i < *count)
+   insert(abc, coord, map, y, x, a);
+   /*  while(i < *count)
     {
     	flg = insert(abc, coord, map, y, x, a);
         i++;
@@ -112,19 +125,8 @@ void    backtrack(int *count, int storage[26], char map[16][17])
          
         i++;
         }
-    }   
-	i = 0;
-	while (i <= a)
-	{
-		j = 0;
-		while (j <= a)
-		{
-			ft_putchar(map[i][j]);
-			j++;
-		}
-		ft_putchar('\n');
-		i++;
-	}
+    }    */
+
 
        /* while (col <= a)        {
             while (row <= a)
