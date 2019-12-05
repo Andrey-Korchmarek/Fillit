b1504a6cb14db8d0bf4b615ac754874fc504198b
aelphias <igormitchell11@gmail.com>
2019-12-05 15:01:56 +0300

    can insert a square properly

--- fillit/backtrack.c
+++ fillit/backtrack.c
@@ -6,17 +6,21 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 14:09:14 by aelphias          #+#    #+#             */
-/*   Updated: 2019/12/04 15:18:26 by aelphias         ###   ########.fr       */
+/*   Updated: 2019/12/05 15:01:29 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+ /* idea board starting size =
+  sqrt(# of tetrominoes * 4 characters per tetromino)
+  
+  i = 1 will be , minimal square 2*2 */
 int min_a(int *count) // "a" means a side of map square
 {
     int i;
-
-    i = 10;
+    
+    i = 2;
     while(i * i < 4 * *count) /* How on earth does it work?)*/
         ++i;
     printf("{ min_a = %d }\n", i - 1);
@@ -35,23 +39,28 @@ void clean_map(char map[16][17])
     }
 }
 
-int    insert(char abc, int *coord, char map[16][17], int a)
+int    insert(int *coord, char map[16][17], int a)
 {   
     int y;
     int x;
-
+    char abc;
+    
+    abc = 'A';
     y = 0;
     while (y <= a )
     {
         x = 0;
 	    while (x <= a)
-        {
-            if (y + coord[0] < a && x + coord[1] < a && y + coord[2] < a && x + coord[3] < a && y + coord[4] < a &&
-            x + coord[5] < a && map[y][x] == '.' && map[coord[0]][coord[1]] == '.' &&
-            map[coord[2]][coord[3]] == '.'  && map[coord[4]][coord[5]] == '.'
-            &&  y + coord[0] > 0 && x + coord[1] > 0 
-            && y + coord[2] > 0 && x + coord[3] > 0 
-            && y + coord[4] > 0 && x + coord[5] > 0 )
+        {//int g_tetramines[19][10] =	{{0, 1, 0, 2, 0, 3}, /* 0*/     
+         if (y + coord[0] <=  a && x + coord[1] <= a && y + coord[2] <= a 
+                && x + coord[3] <= a && y + coord[4] <= a 
+                && x + coord[5] <= a && map[y][x] == '.' 
+                && map[coord[0]][coord[1]] == '.'
+                && map[coord[2]][coord[3]] == '.'  
+                && map[coord[4]][coord[5]] == '.'
+                && y + coord[0] >= 0 && x + coord[1] >= 0 
+                && y + coord[2] >= 0 && x + coord[3] >= 0 
+                && y + coord[4] >= 0 && x + coord[5] >= 0 )
             {
                 map[y][x] = abc;
     	        map[y + coord[0]][x + coord[1]] = abc;
@@ -88,18 +97,10 @@ void print(int a, char map[16][17])
 void    backtrack(int *count, int storage[26], char map[16][17])
 {
     extern int g_tetramines[19][10];
-    int i;
     int a;
-    int j;
- 
- //   int n;
-   
+    
     printf("backtrack count = %d\n", *count);
     a = min_a(count);
-    i = 0;
-    j = 0;
-    i = 0;
-    
-    insert('A', g_tetramines[storage[0]], map, a);
+    insert(g_tetramines[storage[0]], map, a);
     print(a, map);
 } 
